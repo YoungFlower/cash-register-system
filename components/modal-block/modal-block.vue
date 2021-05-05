730d5c0d686d819009c7eb064e5a41d2a4f481ec
YoungFlower <49740106+YoungFlower@users.noreply.github.com>
2021-05-05 15:11:05 +0800

    首页内容更新

--- components/modal-block/modal-block.vue
+++ components/modal-block/modal-block.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class='modal-block'>
+  <div class='modal-block' @click="routerEvent">
+    {{name}}
   </div>
 </template>
 <script>
@@ -7,15 +8,29 @@ export default {
   name: '',
   props: {
     name: {
+      type: [String, Number],
       default: ''
     }
   },
-  data() {},
+  data() {
+    return {}
+  },
   mounted() {},
   methods: {}
 }
 </script>
 <style lang='scss'>
 .modal-block {
+  display: inline-block;
+  font-size: 24rpx;
+  text-align: center;
+  width: 100rpx;
+  height: 100rpx;
+  padding: 18rpx;
+  line-height: 32rpx;
+  margin: 16rpx 33rpx;
+  background: rgba(255, 255, 255, 0.4);
+  color: rgba(0, 0, 0, 0.6);
+  border-radius: 20rpx;
 }
 </style>
